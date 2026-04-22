18f7f2b5ca634c9202f19b5c09716b9ff4c56312
nurse103 <phanduythang@nurse103.pro.vn>
2026-04-23 00:32:11 +0700

    fix: explicit asset redirects and absolute entry path

--- vite.config.ts
+++ vite.config.ts
@@ -27,11 +27,6 @@ export default defineConfig(({ mode }) => {
     build: {
       modulePreload: false,
       target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
-      rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, 'index.html'),
-        },
-      },
     },
     envPrefix: 'VITE_',
     define: {
